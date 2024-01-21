52a6fce5bb99a23a70f0af7ca1d2e276096a2af8
imyroslav <imyroslav@ukr.net>
2024-01-21 21:26:01 +0700

    template-strings

--- js/task-4.js
+++ js/task-4.js
@@ -3,19 +3,19 @@ function getShippingCost(country) {
     switch (country) {
 
         case `Australia`:
-        return "Shipping to Australia will cost 170 credits";
+        return `Shipping to ${country} will cost 170 credits`;
 
         case `Chile`:
-        return "Shipping to Chile will cost 250 credits";
+        return `Shipping to ${country} will cost 250 credits`;
 
         case `Jamaica`:
-        return "Shipping to Jamaica will cost 120 credits";
+        return `Shipping to ${country} will cost 120 credits`;
 
         case `China`:
-        return "Shipping to China will cost 100 credits";
+        return `Shipping to ${country} will cost 100 credits`;
 
         default:
-        return "Sorry, there is no delivery to your country";
+        return `Sorry, there is no delivery to ${country}`;
     
     }
     
